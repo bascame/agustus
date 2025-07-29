63ca6c97b5d03ba343960273a0977fd4734b813c
edwinkurniawan1895 <edwinkurniawan1895@gmail.com>
2025-07-29 21:40:57 +0000

    push

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+import './style.css';
 import { initializeApp } from "firebase/app";
 import { getStorage, ref } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
   // ... your firebase config
-};
+    apiKey: "AIzaSyBNN2z7BvYUhU4Ws1ezYM5nsQ8S276muvw",
+    authDomain: "semarak-kemerdekaan.firebaseapp.com",
+    databaseURL: "https://semarak-kemerdekaan-default-rtdb.firebaseio.com",
+    projectId: "semarak-kemerdekaan",
+    storageBucket: "semarak-kemerdekaan.firebasestorage.app",
+    messagingSenderId: "543406328573",
+    appId: "1:543406328573:web:bb402da63417a8b2570c94"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
